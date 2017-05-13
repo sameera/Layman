52826c1ca3ef39c776355177450f5c3832700608
Sameera Perera <www.sameera@gmail.com>
2017-05-13 06:49:56 +0530

    Ading source files

---
+++ Layman/ShouldExtension.cs
@@ -0,0 +1,82 @@
+﻿namespace Layman
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    public static class should
+    {
+        public static bool throw_a<T>(Action act)
+        {
+            try
+            {
+                act();
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return ex is T;
+            }
+        }
+
+        public static bool not_throw<T>(Action act)
+        {
+            return !throw_a<T>(act);
+        }
+
+        public static bool throw_an_exception(Action act)
+        {
+            try
+            {
+                act();
+                return false;
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+        }
+
+        public static bool not_throw_an_exception(Action act)
+        {
+            return !throw_an_exception(act);
+        }
+    }
+
+    public static class ShouldExtension
+    {
+        public static bool should_be<T>(this T actual, T expected)
+        {
+            return EqualityComparer<T>.Default.Equals(actual, expected);
+        }
+
+        /*
+        public static bool should_be(this object actual, string expected)
+        {
+            return expected.Equals(actual);
+        }
+        */
+
+        public static bool should_not_be<T>(this T actual, T unexpected)
+        {
+            return !unexpected.should_be(actual);
+        }
+
+        /*
+        public static bool should_not_be(this object actual, string unexpected)
+        {
+            return !unexpected.should_be(actual);
+        }
+        */
+
+        public static bool should_match(this object value, string regex)
+        {
+            return value != null && new Regex(regex).IsMatch(value.ToString());
+        }
+
+        public static bool is_a(this object tested, Func<object, bool> check)
+        {
+            return check(tested);
+        }
+    }
+}
---
+++ Layman/TestSpec.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Layman
+{
+    [TestClass]
+    public partial class TestSpec
+    {
+        /*
+        protected void MockLogging()
+        {
+            var config = new LoggingConfiguration();
+            config.AddTarget(new NLog.Targets.NullTarget());
+            NLog.LogManager.Configuration = config;
+        }
+        */
+
+        protected void given(string description, Action setupAction)
+        {
+            Console.WriteLine(description);
+            given(setupAction);
+        }
+
+        protected void given(Action setupAction)
+        {
+            setupAction();
+        }
+
+        protected void when(string description, Action act)
+        {
+            Console.WriteLine(description);
+            when(act);
+        }
+
+        protected void when(Action act)
+        {
+            act();
+        }
+
+        protected void it(string description, Func<bool> check)
+        {
+            Assert.IsTrue(check(), description + " :FAILED:");
+            Console.WriteLine(description);
+        }
+
+        protected void it(Func<bool> check)
+        {
+            it(check.Method.Name, check);
+        }
+
+        protected void it(string description, Action check)
+        {
+            check();
+            Console.WriteLine(description);
+        }
+
+        protected void it(Action check)
+        {
+            check();
+        }
+
+        protected struct Reaction<T>
+        {
+            private readonly T subject;
+
+            public void which_results_in(Action<T> react)
+            {
+                react(subject);
+            }
+
+            public Reaction(T subject)
+            {
+                this.subject = subject;
+            }
+        }
+    }
+}
